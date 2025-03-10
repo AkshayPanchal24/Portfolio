c98ae8801c8ddddf07dbb417704a7fbec1c99c89
Akshay Panchal <86816808+Akki0p@users.noreply.github.com>
2025-03-10 15:51:15 +0530

    Initial commit

---
+++ src/data/experiences.js
@@ -0,0 +1,45 @@
+import genio from "../assets/images/genio.jpeg";
+import draic from "../assets/images/image2.png";
+import rbl from "../assets/images/RBL.png";
+import stealth from "../assets/images/stealth.jpeg";
+
+import { blue, green, pink, yellow } from "../utils";
+
+export const experiences = [
+  {
+    logo: stealth,
+    name: "Agamverse",
+    joined: "Sept'24",
+    end: "Currently",
+    title: "Virtual Reality Developer",
+    bio: "Utilized Unity for VR development, implementing optimized rendering techniques to enhance performance and user immersion. Integrated VR interaction systems for intuitive user experiences. Developed an in-engine admin panel for real-time data visualization and scene management.",
+    color: blue,
+  },
+  {
+    logo: rbl,
+    name: "Tomato Games",
+    joined: "January'24",
+    end: "July'24",
+    title: "Unity Developer",
+    bio: "Collaborated with mentors and teammates to brainstorm ideas for game mechanics and features. Developed backend systems for game data management and user authentication using Unity and C#.",
+    color: green,
+  },
+  {
+    logo: genio,
+    name: "L&T Defence",
+    joined: "Aug'21",
+    end: "Jan'24",
+    title: "Jr. Engineer",
+    bio: "Worked on various defense projects involving armored systems at L&T Defence Armoured Complex. Gained hands-on experience in mechanical and electrical systems, contributing to the development and optimization of defense-grade engineering solutions.",
+    color: yellow,
+  },
+  {
+    logo: draic,
+    name: "infoLabz",
+    joined: "Sept'23",
+    end: "Oct'23",
+    title: "Internship",
+    bio: "In D.Y. Patil Robotics and A.I. club we developed collaborative skills. Built amazing projects and brainstormed on various problem statements",
+    color: pink,
+  },
+];
---
+++ src/pages/About/About.jsx
@@ -0,0 +1,90 @@
+import { Canvas } from "@react-three/fiber";
+import gsap from "gsap";
+import Flip from "gsap/Flip";
+import React, { useEffect } from "react";
+import { Page } from "../../components/Page";
+import { blue, green, yellow } from "../../utils";
+import { Educations, Paragraph, SkillsWrapper, Text } from "./About.styled";
+import { AboutItem } from "./AboutItem";
+import Skills from "./SkillBall";
+import dyp from "../../assets/images/VDP.jpeg";
+import highschool from "../../assets/images/UTU.png";
+import sos from "../../assets/images/sos.png";
+import { useInView } from "react-intersection-observer";
+import { useState } from "react";
+
+export const About = () => {
+  const { ref, inView } = useInView({});
+  const [show, setShow] = useState(inView);
+  useEffect(() => {
+    setShow(inView);
+  }, [inView]);
+
+  useEffect(() => {
+    gsap.registerPlugin(Flip);
+    let cards = document.querySelectorAll(".about-item");
+    cards.forEach((card, i) => {
+      if (i === 0) {
+        card.classList.add("active");
+      }
+      card.addEventListener("mouseenter", (e) => {
+        if (card.classList.contains("active")) {
+          return;
+        }
+        const state = Flip.getState(cards);
+        cards.forEach((c) => {
+          c.classList.remove("active");
+        });
+        card.classList.add("active");
+        Flip.from(state, {
+          duration: 0.5,
+          ease: "elastic.out(1,0.9)",
+          absolute: true,
+        });
+      });
+    });
+  }, []);
+  return (
+    <div ref={ref}>
+      <Page header="About">
+        <Text>
+          <Paragraph>
+            I'm a passionate programmer, who is always looking for new
+            challenges to improve myself, also a team player, who is always
+            ready to learn new things and help others.
+            <br />I was born and raised in Surat, India. I love to play Games
+            and Listen Hiphop and in my free time I like to read books and watch movies.
+          </Paragraph>
+          <Educations>
+            <AboutItem
+              color={blue}
+              active
+              data={{
+                title: "Vidhyadeep University, Surat",
+                p: "Computer Engineering (2021-2024)",
+                image: dyp,
+              }}
+            />
+            <AboutItem
+              color={green}
+              data={{
+                title: "Uka Tarsadia University",
+                p: "Mechanical Engineering (2018-2021)",
+                image: highschool,
+              }}
+            />
+          </Educations>
+        </Text>
+        <SkillsWrapper>
+          {show ? (
+            <Canvas camera={{ position: [0, 0, 18] }}>
+              <Skills />
+            </Canvas>
+          ) : (
+            `${inView}`
+          )}
+        </SkillsWrapper>
+      </Page>
+    </div>
+  );
+};
